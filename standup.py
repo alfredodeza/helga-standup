b37a8c4052a778d33530a3cb704fec745d013ff7
Alfredo Deza <alfredo@deza.pe>
2017-10-10 11:27:21 -0400

    allow adding statuses to the database

--- standup.py
+++ standup.py
@@ -1,96 +1,131 @@
 import random
 import uuid
 from helga.plugins import command
+from helga.db import db
 from helga import log
 
 logger = log.getLogger(__name__)
 
 
 bad_statuses = {
-        'clueless': [
-            (
-                'I',
-                '',
-                'hrmn...',
-            ),
-            (
-                "can't",
-                "don't",
-            ),
-            (
-                'recollect',
-                'recall',
-                'remember',
-                'relate',
-                'connect',
-                'cite',
-                'refer',
-                'associate',
-            ),
-            (
-                'yesterday',
-                'anything',
-                'this morning',
-                'Friday',
-            )
-        ],
-        'busy': [
-            (
-                'working',
-                'blocked',
-                'baffled',
-                'puzzled',
-                'frustrated',
-                'flummoxed',
-                'stupefied',
-                'dumfounded',
-                'befuddled',
-                'terrified',
-                'discombobulated',
-                'vexed'
-            ),
-            (
-                'on',
-                'with',
-                'at',
-            ),
-            (
-                'issues',
-                'tickets',
-            ),
-            (
-                ', '.join([str(uuid.uuid4())[:8].upper() for i in range(random.randint(2, 5))]),
-                ', '.join([str(uuid.uuid4())[:8].upper() for i in range(random.randint(2, 5))])
-            )
-        ],
-        'literal': [
-            (
-                'I have a couple of questions about',
-                'need to discuss more on the',
-                'lets talk ',
-                'we need to determine what is going on with the',
-                'quick chat about',
-            ),
-            (
-                'Riemann theorem',
-                "Jacobi's elliptic functions",
-                'Ahlfors theory',
-                'Value distribution theory of holomorphic functions',
-                'Hadamard three-circle theorem',
-                'Bieberbach conjecture',
-                "Landau's constants",
-                "Schwarzian derivative",
-            ),
-        ]
-    }
-
+    'clueless': [
+        (
+            'I',
+            '',
+            'hrmn...',
+        ),
+        (
+            "can't",
+            "don't",
+        ),
+        (
+            'recollect',
+            'recall',
+            'remember',
+            'relate',
+            'connect',
+            'cite',
+            'refer',
+            'associate',
+        ),
+        (
+            'yesterday',
+            'anything',
+            'this morning',
+            'Friday',
+        )
+    ],
+    'busy': [
+        (
+            'working',
+            'blocked',
+            'baffled',
+            'puzzled',
+            'frustrated',
+            'flummoxed',
+            'stupefied',
+            'dumfounded',
+            'befuddled',
+            'terrified',
+            'discombobulated',
+            'vexed'
+        ),
+        (
+            'on',
+            'with',
+            'at',
+        ),
+        (
+            'issues',
+            'tickets',
+        ),
+        (
+            ', '.join([str(uuid.uuid4())[:8].upper() for i in range(random.randint(2, 5))]),
+            ', '.join([str(uuid.uuid4())[:8].upper() for i in range(random.randint(2, 5))])
+        )
+    ],
+    'literal': [
+        (
+            'I have a couple of questions about',
+            'need to discuss more on the',
+            'lets talk ',
+            'we need to determine what is going on with the',
+            'quick chat about',
+        ),
+        (
+            'Riemann theorem',
+            "Jacobi's elliptic functions",
+            'Ahlfors theory',
+            'Value distribution theory of holomorphic functions',
+            'Hadamard three-circle theorem',
+            'Bieberbach conjecture',
+            "Landau's constants",
+            "Schwarzian derivative",
+        ),
+    ]
+}
 
 
 @command('standup',
          help='say a completely unacceptable thing for a standup status')
 def standup(client, channel, nick, message, *args):
-    key = random.choice(bad_statuses.keys())
-    phrase = ''
-    for item in bad_statuses[key]:
-        phrase += random.choice(item) + ' '
-    return phrase
+    if len(args) == 0:
+        # make a phrase
+        phrases = []
+        key = random.choice(bad_statuses.keys())
+        phrase = ''
+        for item in bad_statuses[key]:
+            phrase += random.choice(item) + ' '
+
+        phrases.append(phrase)
+
+        count = db.yelling.count()
+        if count:
+            status = db.standup.find()[random.randrange(count)]
+            phrases.append(status['msg'])
+
+        client.msg(channel, random.choice(phrases))
+
+    if len(args) == 2:
+        cmd, status = args[1][0], args[1][1:]
+
+        if cmd == 'add':
+            db.standup.update_one({
+                'msg': message,
+            }, {'$set': {'msg': message}}, upsert=True)
+
+        if cmd == 'remove':
+            status_to_forget = ' '.join(status)
+
+            logger.debug('will attempt to purge: {}'.format(status_to_forget))
+
+            remove_result = db.standup.delete_one({
+                'msg': status_to_forget,
+            })
+
+            logger.debug('remove_result: {}'.format(remove_result.deleted_count))
+
+            if remove_result.deleted_count == 1:
+                return 'done.'
+            else:
+                return 'not found :('
